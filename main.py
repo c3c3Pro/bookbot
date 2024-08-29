dbf4d73d7bb124261f50c3b31a5311b0bd98ba8d
c3c3Pro <c3lina.bitari@gmail.com>
2024-08-29 22:14:18 +0400

    added word count to the text file

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from pdb import main
 
+def count(t):
+    total_number_of_words = t.split()
+    return len(total_number_of_words)
+
 def main():
 
     file_path = "books/frankenstein.txt"
     with open(file_path, 'r') as f:
-    #write the code here 
+    #reads the file frankenstein
         file_contents = f.read()
         print(file_contents)
+        number_words = count(file_contents)
+        print(f"total number of words: {number_words}")
 
 if __name__ == "__main__":
     main()
