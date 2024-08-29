9ddfd648455a4d83b87b15c4177b3149c14a6a67
c3c3Pro <c3lina.bitari@gmail.com>
2024-08-29 21:44:47 +0400

    fixed the read path

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from pdb import main
 
+def main():
 
-with open("books/frankenstein.txt") as f:
+    file_path = "books/frankenstein.txt"
+    with open(file_path, 'r') as f:
     #write the code here 
-    file_contents = f.read()
-    print(file_contents)
+        file_contents = f.read()
+        print(file_contents)
 
 if __name__ == "__main__":
     main()
