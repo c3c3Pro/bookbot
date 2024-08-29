33ab7afd6201543cb794393749589d8e81e30a22
c3c3Pro <c3lina.bitari@gmail.com>
2024-08-29 18:47:23 +0400

    created a read file

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+from pdb import main
+
+
+with open("books/frankenstein.txt") as f:
+    #write the code here 
+    file_contents = f.read()
+    print(file_contents)
+
+if __name__ == "__main__":
+    main()
